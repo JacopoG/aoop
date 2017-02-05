b7da7b38ed11a96c85600c51af3ed67df2dead73
Jacopo <jacopo.gargiulo@icloud.com>
2017-02-05 17:05:04 +0100

    Sensor OOP

---
+++ Sensor.h
@@ -0,0 +1,71 @@
+void SensorFakeCallback () {
+}
+
+class Sensor;
+
+typedef (*sensorCallback)(Sensor);
+
+class Sensor {
+
+  public:
+
+
+
+    int pin;
+
+    sensorCallback onHighCallback;
+
+    sensorCallback onLowCallback;
+
+    sensorCallback onChangeStatusCallback;
+
+    bool pastStatus;
+
+
+
+    void init (int _pin, bool pullup = true) {
+      pin = _pin;
+
+      if (pullup) {
+        pinMode(pin, INPUT_PULLUP);
+      }
+      else {
+        pinMode(pin, INPUT);
+      }
+
+      pastStatus = getStatus();
+
+      onHighCallback = SensorFakeCallback;
+      onLowCallback = SensorFakeCallback;
+      onChangeStatusCallback = SensorFakeCallback;
+    };
+
+
+
+    bool getStatus () {
+      return digitalRead(pin);
+    }
+
+
+
+    void run () {
+      bool newStatus = getStatus();
+      if (pastStatus != newStatus) {
+        onChangeStatusCallback(*this);
+
+        if (newStatus == HIGH) {
+          onHighCallback(*this);
+        }
+
+        if (newStatus == LOW) {
+          onLowCallback(*this);
+        }
+      }
+
+      pastStatus = newStatus;
+    };
+
+
+    
+};
+
